f81e4cb0dd43e2516f837068320a25e4be128aa7
Eventttt <153360260+Eventttt@users.noreply.github.com>
2025-04-23 00:59:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I love you too ginaa💞";
+  question.innerHTML = "Aaaaa, I love you too ginaa";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
