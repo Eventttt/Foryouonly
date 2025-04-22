8339922c2af653f0ce05873530dab65089568311
Eventttt <153360260+Eventttt@users.noreply.github.com>
2025-04-23 01:06:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I love you too ginaa";
+  question.innerHTML = "Aaaaa, I love you too ginaa💞";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
